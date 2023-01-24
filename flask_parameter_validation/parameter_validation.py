79f6142ee480470f2c6b51f13bf9e27cb0185f57
George O <16269580+Ge0rg3@users.noreply.github.com>
2023-01-24 18:32:18 +0400

    Minor styling refactor

--- flask_parameter_validation/parameter_validation.py
+++ flask_parameter_validation/parameter_validation.py
@@ -4,11 +4,11 @@ from flask import request
 from inspect import signature
 from werkzeug.exceptions import BadRequest
 
+
 class ValidateParameters:
 
     def __init__(self, error_handler=None):
         self.custom_error_handler = error_handler
-
 
     def __call__(self, f):
         """
@@ -16,6 +16,7 @@ class ValidateParameters:
         """
         def nested_func(**kwargs):
             # Step 1 - Combine all flask input types to one dict
+            json_input = None
             if request.headers.get("content-type") == "application/json":
                 try:
                     json_input = request.json
@@ -23,7 +24,7 @@ class ValidateParameters:
                     return {"error": "Could not parse JSON."}, 400
             request_inputs = {
                 Route: kwargs.copy(),
-                Json: json_input if 'json_input' in vars() else None,
+                Json: json_input,
                 Query: request.args.to_dict(),
                 Form: request.form.to_dict(),
                 File: request.files.to_dict()
@@ -59,7 +60,8 @@ class ValidateParameters:
         # Extract useful information from expected input
         expected_name = expected_input.name
         expected_input_type = expected_input.annotation  # i.e. str, int etc.
-        expected_delivery_type  = expected_input.default  # i.e. Form, Query, Json etc.
+        # i.e. Form, Query, Json etc.
+        expected_delivery_type = expected_input.default
 
         # Get input type as string to recognize typing objects, e.g. to convert typing.List to "typing.List"
         # Note: We use this str() method, as typing API is too unreliable, see https://stackoverflow.com/a/52664522/7173479
@@ -75,7 +77,8 @@ class ValidateParameters:
             raise InvalidParameterTypeError(expected_delivery_type)
 
         # Validate that user supplied input in expected delivery type (unless specified as Optional)
-        user_input = all_request_inputs[expected_delivery_type.__class__].get(expected_name)
+        user_input = all_request_inputs[expected_delivery_type.__class__].get(
+            expected_name)
         if user_input is None:
             # If default is given, set and continue
             if expected_delivery_type.default is not None:
@@ -85,8 +88,9 @@ class ValidateParameters:
                 if hasattr(expected_input_type, "__args__") and type(None) in expected_input_type.__args__:
                     return user_input
                 else:
-                    raise MissingInputError(expected_name, expected_delivery_type.__class__)
-        
+                    raise MissingInputError(
+                        expected_name, expected_delivery_type.__class__)
+
         # Skip validation if typing.Any is given
         if expected_input_type_str.startswith("typing.Any"):
             return user_input
@@ -96,7 +100,6 @@ class ValidateParameters:
             new_type = expected_input_type.__args__[0]
             expected_input_type = new_type
             expected_input_type_str_str = str(new_type)
-
 
         # Prepare expected type checks for unions, lists and plain types
         if expected_input_type_str.startswith("typing.Union"):
@@ -122,32 +125,35 @@ class ValidateParameters:
         else:
             user_inputs = [user_input]
             expected_input_types = [expected_input_type]
-        
+
         # Perform automatic type conversion for parameter types (i.e. "true" -> True)
         for count, value in enumerate(user_inputs):
-            user_inputs[count] = expected_delivery_type.convert(value, expected_input_types)
+            user_inputs[count] = expected_delivery_type.convert(
+                value, expected_input_types)
 
         # Validate that user type(s) match expected type(s)
-        validation_success = all(type(inp) in expected_input_types for inp in user_inputs)
+        validation_success = all(
+            type(inp) in expected_input_types for inp in user_inputs)
 
         # Validate that if lists are required, lists are given
         if expected_input_type_str.startswith("typing.List"):
             if type(user_input) is not list:
                 validation_success = False
-        
+
         # Error if types don't match
         if not validation_success:
             if hasattr(original_expected_input_type, "__name__") and not original_expected_input_type_str.startswith("typing."):
                 type_name = original_expected_input_type.__name__
             else:
                 type_name = original_expected_input_type_str
-            raise ValidationError(f"must be type '{type_name}'", expected_name, original_expected_input_type)
+            raise ValidationError(
+                f"must be type '{type_name}'", expected_name, original_expected_input_type)
 
         # Validate parameter-specific requirements are met
         try:
             expected_delivery_type.validate(user_input)
         except ValueError as e:
             raise ValidationError(str(e), expected_name, expected_input_type)
-        
+
         # Return input back to parent function
         return user_input
