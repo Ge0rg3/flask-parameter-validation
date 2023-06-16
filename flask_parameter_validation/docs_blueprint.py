fd895960605dbcfb9ac7d276ef62a180e38c069c
Seth Teichman <steichman@d3engineering.com>
2023-06-16 12:03:36 -0400

    Add comments to docs_blueprint.py

--- flask_parameter_validation/docs_blueprint.py
+++ flask_parameter_validation/docs_blueprint.py
@@ -9,23 +9,20 @@ docs_blueprint = Blueprint("docs", __name__, url_prefix="/docs", template_folder
 def docs_html():
     config = flask.current_app.config
     docs_arr = []
-    for rule in current_app.url_map.iter_rules():
+    for rule in current_app.url_map.iter_rules():  # Iterate through all Flask Routes
         this_docs = dict()
-        methods_str = "["
-        for method in rule.methods:
-            if method not in ["OPTIONS", "HEAD"]:
-                methods_str += method + ", "
-        methods_str = methods_str[0:-2] + "] "
-        print(methods_str + str(rule))
-        rule_func = current_app.view_functions[rule.endpoint]
-        fn_list = ValidateParameters().get_fn_list()
-        this_docs["rule"] = str(rule)
+        rule_func = current_app.view_functions[rule.endpoint]  # Get the function that the Flask Route calls
+        fn_list = ValidateParameters().get_fn_list()  # Get the function list that ValidateParameters accumulates
+        this_docs["rule"] = str(rule)  # Get the URL rule for this Route
+        # Gather all HTTP methods applicable to this Route
         this_docs["methods"] = []
         for method in rule.methods:
             this_docs["methods"].append(str(method))
-        for fsig, fdocs in fn_list.items():
+        for fsig, fdocs in fn_list.items():  # Iterate through all functions ValidateParameters decorates
+            # Find the ValidateParameters function for rule_func - this will find nothing if ValidateParameters isn't
+            # the bottommost decorator (confirmation needed), or if ValidateParameters isn't present at all
             if fsig.endswith(rule_func.__name__):
-                if fdocs["docstring"] is not None:
+                if fdocs["docstring"] is not None:  # Add docstring to result in HTML format, do not sanitize tags
                     docstring = fdocs["docstring"]
                     while docstring.startswith("\n"):
                         docstring = docstring[1:]
@@ -34,16 +31,14 @@ def docs_html():
                 else:
                     this_docs["docstring"] = None
                 this_docs["args"] = dict()
-                i = 0
-                if fdocs["docstring"] is not None:
-                    print("'''" + fdocs["docstring"] + "'''")
-                for arg in fdocs["argspec"].args:
-                    this_arg = dict()
-                    this_arg["name"] = arg
+                for i in range(len(fdocs["argspec"].args)):  # Iterate through arguments captured by ValidateParameters
+                    arg = fdocs["argspec"].args[i]
+                    this_arg = dict()  # Store argument data as a dictionary
+                    this_arg["name"] = arg  # Argument Name
                     arg_print = "    " + arg + ": "
-                    arg_annots = fdocs["argspec"].annotations[arg]
+                    arg_annots = fdocs["argspec"].annotations[arg]  # Argument Type Hint
                     arg_print_type = arg_annots.__name__
-                    if hasattr(arg_annots, "__args__"):
+                    if hasattr(arg_annots, "__args__"):  # Sub-types of Optional, Union, list, etc.
                         arg_print_type += "["
                         for annot_arg in arg_annots.__args__:
                             arg_print_type += str(annot_arg.__name__) + ", "
@@ -51,33 +46,27 @@ def docs_html():
                     this_arg["type"] = arg_print_type
                     arg_print += arg_print_type + " in request "
                     arg_loc = fdocs["argspec"].defaults[i]
-                    this_arg["loc"] = type(arg_loc).__name__
+                    this_arg["loc"] = type(arg_loc).__name__  # Where in the Request the Argument comes from, one of FPV's Parameter classes
                     arg_print += type(arg_loc).__name__
                     # print(arg_loc.__dict__)
                     arg_loc_has_params = False
                     this_arg["loc_args"] = dict()
-                    for param in arg_loc.__dict__.keys():
-                        if arg_loc.__dict__[param] is not None and arg_loc_has_params is False:
-                            arg_loc_has_params = True
-                            arg_print += " ("
-                            if not callable(arg_loc.__dict__[param]):
-                                this_arg["loc_args"][param] = arg_loc.__dict__[param]
-                            else:
+                    for param in arg_loc.__dict__.keys():  # Iterate through arguments to the Parameter constructor
+                        if arg_loc.__dict__[param] is not None:
+                            if arg_loc_has_params is False:
+                                arg_loc_has_params = True
+                                arg_print += " ("
+                            if not callable(arg_loc.__dict__[param]):  # For non-callable arguments (most arguments)
+                                this_arg["loc_args"][param] = arg_loc.__dict__[param]  # Just save it to the dict as is
+                            else:  # For callable arguments (func), convert it to the module path and function name
                                 this_arg["loc_args"][param] = arg_loc.__dict__[param].__module__+"."+arg_loc.__dict__[param].__name__
-                        elif arg_loc.__dict__[param] is not None:
-                            arg_print += param + "=" + str(arg_loc.__dict__[param]) + ", "
-                            if not callable(arg_loc.__dict__[param]):
-                                this_arg["loc_args"][param] = arg_loc.__dict__[param]
-                            else:
-                                this_arg["loc_args"][param] = arg_loc.__dict__[param].__module__+"."+arg_loc.__dict__[param].__name__
-                    if this_arg["loc"] in this_docs["args"]:
+                    if this_arg["loc"] in this_docs["args"]:  # Does this function aleady have an argument list for this Parameter class?
                         this_docs["args"][this_arg["loc"]].append(this_arg)
                     else:
                         this_docs["args"][this_arg["loc"]] = [this_arg]
                     if arg_loc_has_params:
                         arg_print = arg_print[0:-2] + ")"
                     print(arg_print)
-                    i+=1
 
         docs_arr.append(this_docs)
     return flask.render_template("fpv_default_docs.html",
