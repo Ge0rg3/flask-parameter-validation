2eabd6d7721c6c7ea50fb88c28fc577c675e54fa
George O <16269580+Ge0rg3@users.noreply.github.com>
2023-09-11 11:48:25 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='Flask-Parameter-Validation',
-    version='2.1.8',
+    version='2.1.9',
     url='https://github.com/Ge0rg3/flask-parameter-validation',
     license='MIT',
     author='George Omnet',
