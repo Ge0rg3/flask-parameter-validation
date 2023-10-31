97c40e39ae90b2abe00860483238a0e9c1747637
summersz <rasummersr@gmail.com>
2023-10-31 14:41:41 +0000

    feat: apply automatic type conversion to forms
    closes #25

--- flask_parameter_validation/parameter_types/form.py
+++ flask_parameter_validation/parameter_types/form.py
@@ -2,10 +2,10 @@
     Values sent in a POST Form request
     - Typical data from an HTML form (non-json)
 """
-from .parameter import Parameter
+from .query import Query
 
 
-class Form(Parameter):
+class Form(Query):
     name = "form"
 
     def __init__(self, default=None, **kwargs):
