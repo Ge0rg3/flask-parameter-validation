464e84a6e31efbfca8d46c1be1339a92453863fe
Seth Teichman <smt5541@rit.edu>
2024-02-06 17:51:34 -0500

    Add tests for required and optional str, and str default and min_str_length

--- flask_parameter_validation/test/test_query_params.py
+++ flask_parameter_validation/test/test_query_params.py
@@ -1,7 +1,36 @@
-def test_str(client):
-    r = client.get("/query/str?value=value")
-    json = r.json
-    assert "value" in json
-    assert json["value"] == "value"
-    r = client.get("/query/str")
-    print(r.text)
+def test_required_str(client):
+    r = client.get("/query/required_str?value=value")
+    assert "value" in r.json
+    assert r.json["value"] == "value"
+    r = client.get("/query/required_str")
+    assert "error" in r.json
+
+def test_optional_str(client):
+    r = client.get("/query/optional_str")
+    assert "value" in r.json
+    assert r.json["value"] is None
+    r = client.get("/query/optional_str?value=value")
+    assert "value" in r.json
+    assert r.json["value"] == "value"
+
+def test_str_default(client):
+    r = client.get("/query/str_default")
+    assert "optional" in r.json
+    assert r.json["optional"] == "optional"
+    assert "not_optional" in r.json
+    assert r.json["not_optional"] == "not_optional"
+    r = client.get("/query/str_default?optional=a&not_optional=b")
+    assert "optional" in r.json
+    assert r.json["optional"] == "a"
+    assert "not_optional" in r.json
+    assert r.json["not_optional"] == "b"
+
+def test_str_min_str_length(client):
+    r = client.get("/query/str_min_str_length?v=")
+    assert "error" in r.json
+    r = client.get("/query/str_min_str_length?v=a")
+    assert "v" in r.json
+    assert r.json["v"] == "a"
+    r = client.get("/query/str_min_str_length?v=aa")
+    assert "v" in r.json
+    assert r.json["v"] == "aa"
--- flask_parameter_validation/test/testing_application.py
+++ flask_parameter_validation/test/testing_application.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from flask import Flask, jsonify
 
 from flask_parameter_validation import ValidateParameters, Query
@@ -5,9 +7,32 @@ from flask_parameter_validation import ValidateParameters, Query
 def create_app():
     app = Flask(__name__)
 
-    @app.get("/query/str")
+    @app.get("/query/required_str")
     @ValidateParameters()
-    def query_str(value: str = Query()):
+    def query_required_str(value: str = Query()):
         return jsonify({"value": value})
+
+    @app.get("/query/optional_str")
+    @ValidateParameters()
+    def query_optional_str(value: Optional[str] = Query()):
+        return jsonify({"value": value})
+
+    @app.get("/query/str_default")
+    @ValidateParameters()
+    def query_str_default(
+            not_optional: str = Query(default="not_optional"),
+            optional: Optional[str] = Query(default="optional")
+    ):
+        return jsonify({
+            "not_optional": not_optional,
+            "optional": optional
+        })
+
+    @app.get("/query/str_min_str_length")
+    @ValidateParameters()
+    def query_str_min_str_length(
+            v: str = Query(min_str_length=1)
+    ):
+        return jsonify({"v": v})
 
     return app
